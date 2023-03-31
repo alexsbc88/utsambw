5b7e2eb470279b1810e36faecbfcfd2c61717315
alexsbc88 <billycuan88@gmail.com>
2023-03-31 10:21:31 +0700

    a

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var title = document.querySelector('.title');
-var courseFeatureElements = document.querySelectorAll('.course-feature');
-var button = document.querySelector('button');
 
 navigator.serviceWorker.register('/sw.js');
 
